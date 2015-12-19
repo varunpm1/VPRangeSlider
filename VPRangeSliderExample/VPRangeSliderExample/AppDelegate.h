1927d2e8b41a7c6e0f4f03172f2cb1154ad30d89
Varun P M <varun.pm@ymedialabs.com>
2015-12-19 21:05:15 +0530

    Added example project file for Range Slider

---
+++ VPRangeSliderExample/VPRangeSliderExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VPRangeSliderExample
+//
+//  Created by Varun P M on 19/12/15.
+//  Copyright © 2015 VPN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VPRangeSliderExample/VPRangeSliderExample/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  VPRangeSliderExample
+//
+//  Created by Varun P M on 19/12/15.
+//  Copyright © 2015 VPN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
